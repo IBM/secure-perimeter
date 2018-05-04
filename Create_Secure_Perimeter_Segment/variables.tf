f15b78ce34e98209b5a5886009d3b53c28592e99
Christopher Twomey <christwomey@ie.ibm.com>
2018-05-04 15:37:04 +0100

    Removed vlan create automation from Create_SPS

--- Create_Secure_Perimeter_Segment/variables.tf
+++ Create_Secure_Perimeter_Segment/variables.tf
@@ -1,11 +1,16 @@
 variable "bluemix_api_key" {
   description = "Your IBM Cloud API key. You can get the value by running bx iam api-key-create <key name>."
-  default = ""
 }
 
 variable "gateway_name_id" {
   description = "The name or ID of the gateway to deploy segment into"
-  default = ""
+}
+
+variable "public_vlan_name" {
+    description = "Public vlan name"
+}
+variable "private_vlan_name" {
+    description = "Private vlan name"
 }
 
 variable "vlan_subnet_size" {
@@ -15,15 +20,12 @@ variable "vlan_subnet_size" {
 
 variable "org" {
   description = "Your IBM Cloud org name."
-  default = ""
 }
 
 variable "space" {
   description = "Your IBM Cloud space name."
-  default = ""
 }
 
 variable "region" {
   description = "The IBM Cloud region where you want to deploy your cluster for example us-south."
-  default = ""
 }
