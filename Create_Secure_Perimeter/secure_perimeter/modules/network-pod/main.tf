2d955d44145f9e24ad42be93f1bd2d38c164f1c6
Louise O'Halloran <louise.ohalloran@ie.ibm.com>
2018-05-22 16:20:51 +0100

    changing create pod to be yaml instead of kube provider

--- Create_Secure_Perimeter/secure_perimeter/modules/network-pod/main.tf
+++ Create_Secure_Perimeter/secure_perimeter/modules/network-pod/main.tf
@@ -12,43 +12,16 @@ provider "kubernetes" {
 }
 
 
-
-
 resource "null_resource" "create_pvc" {
   provisioner "local-exec" {
         command = "kubectl --kubeconfig '${var.cluster_config_path}' apply -f ${path.module}/pvc.yaml"
      }
 }
 
-
-
-resource "kubernetes_pod" "network-pod" {
-  metadata {
-    name = "network-pod"
-    namespace = "sp-monitoring"
-
-    labels {
-      app = "network-pod"
-    }
-  }
-
-  spec {
-    container {
-      image = "registry.bluemix.net/ibm/ibmcloud-secure-perimeter-network:1.0.0"
-      name  = "network-pod"
-      volume_mount {
-        name = "network-vol"
-        mount_path = "/opt/secure-perimeter"
-      }
-    }
-    volume {
-      name = "network-vol"
-      persistent_volume_claim {
-        claim_name = "network-pvc"
-    }
-
-    }
-  }
+resource "null_resource" "create_pod" {
+  provisioner "local-exec" {
+        command = "kubectl --kubeconfig '${var.cluster_config_path}' create -f ${path.module}/network-pod.yaml"
+     }
 }
 
 
@@ -56,19 +29,19 @@ resource "kubernetes_pod" "network-pod" {
 #Copy in the ssh key file and the config.json file
 
 resource "null_resource" "copy_files_to_network-pod" {
-   depends_on = ["kubernetes_pod.network-pod"]
+   depends_on = ["null_resource.create_pod"]
 
    provisioner "local-exec" {
-        command = "kubectl  --kubeconfig=${var.cluster_config_path} cp  ${path.root}/keys sp-monitoring/network-pod:/opt/secure-perimeter/"
+        command = "kubectl  --kubeconfig=${var.cluster_config_path} cp  ${path.root}/keys network-pod:/opt/secure-perimeter/"
      }
    provisioner "local-exec" {
-        command = "kubectl  --kubeconfig=${var.cluster_config_path} cp  ${path.root}/state.json sp-monitoring/network-pod:/opt/secure-perimeter/state.json"
+        command = "kubectl  --kubeconfig=${var.cluster_config_path} cp  ${path.root}/state.json network-pod:/opt/secure-perimeter/state.json"
      }
    provisioner "local-exec" {
-        command = "kubectl  --kubeconfig=${var.cluster_config_path} cp ${path.root}/config.json sp-monitoring/network-pod:/opt/secure-perimeter/config.json"
+        command = "kubectl  --kubeconfig=${var.cluster_config_path} cp ${path.root}/config.json network-pod:/opt/secure-perimeter/config.json"
      }
    provisioner "local-exec" {
-        command = "kubectl  --kubeconfig=${var.cluster_config_path} cp ${path.root}/rules.conf sp-monitoring/network-pod:/opt/secure-perimeter/rules.conf"
+        command = "kubectl  --kubeconfig=${var.cluster_config_path} cp ${path.root}/rules.conf network-pod:/opt/secure-perimeter/rules.conf"
      }
 
 }
